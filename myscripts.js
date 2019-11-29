63f7f5a9a3de842f904f3df88c59d3349b8a16e9
Naomi Bashkansky <35845622+nomichka@users.noreply.github.com>
2019-11-29 08:26:31 -0800

    Create myscripts.js

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+function clicked(){
+  var elem = document.getElementById("myButton1");
+  switch (elem.value) {
+    case "<(v.v)>":
+      elem.value = "(v0.0)v  <(^.^<)";
+    case "(v0.0)v <(^.^<)":
+      elem.value = "(/^.^)/\\(^.^\\)";
+    case "(/^.^)/\\(^.^\\)":
+      elem.value = "<(v.v)>";
+}  
