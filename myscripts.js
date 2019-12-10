1195da210eb83c5a0941ab1c5e776d3d1732e49e
Naomi Bashkansky <35845622+nomichka@users.noreply.github.com>
2019-12-10 14:52:42 -0800

    Making next function

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
-function clicked(){
+function clicked() {
   var elem = document.getElementById("myButton1");
   switch (elem.value) {
     case "<(v.v)>":
@@ -11,3 +11,21 @@ function clicked(){
       elem.value = "<(v.v)>";
   }
 }  
+
+function next() {
+  var paragraphs = [
+    document.getElementById("welcome1").style.display === "none",
+    document.getElementById("welcome2").style.display === "none",
+    document.getElementById("welcome3").style.display === "none",
+    document.getElementById("welcome4").style.display === "none",
+    document.getElementById("problem").style.display === "none",
+    document.getElementById("after").style.display === "none"
+  ];
+  
+  var i = paragraphs.indexOf("false");
+  paragraphs[i].style.display = "none";
+  if (i < paragraphs.length) {
+    paragraphs[i + 1].style.display = "block";
+  }
+  
+}
