e977b593f8c21c645919d39b0b51a8d6c3fbc284
Naomi Bashkansky <35845622+nomichka@users.noreply.github.com>
2019-12-16 14:16:02 -0800

    deleting alerts

--- myscripts.js
+++ myscripts.js
@@ -13,11 +13,9 @@ function clicked() {
       elem.innerHTML = "<(v.v)>";
       elem.value = "1";
   }
-  alert('hi');
 }
 
 function move(num) { 
-  alert('inside');
   if (document.getElementById('next').style.display === 'none') {
   	 document.getElementById('next').style.display = "inline-block";
   }
